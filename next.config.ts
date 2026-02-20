9d6191f4e828a8877bba034f50625159ee1b2553
vararais <varadigmaisnann@gmail.com>
2026-02-20 13:24:48 +0700

    Mengubah domain agar dianggap aman

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co', // Izinkan domain ImgBB
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'googleusercontent.com', // Izinkan domain Google (jika masih mau coba Drive)
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
